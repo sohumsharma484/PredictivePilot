04f8b26fcce70df6a7af5e7de3385bb9692a038e
Alexandra Levinshteyn <al68@illinois.edu>
2024-03-15 03:32:42 +0000

    Added txt file to matrix conversion/Added matrix analysis functions

--- src/data_mod/data_conversion_test.py
+++ src/data_mod/data_conversion_test.py
@@ -1,6 +1,6 @@
-import pytest
 import data_conversion
 import numpy as np
+import pytest
 
 def test_determine_file_extension():
     assert data_conversion.determine_file_extension("test.txt") == "txt"
@@ -8,16 +8,24 @@ def test_determine_file_extension():
     assert data_conversion.determine_file_extension("testingtesting123.xlsx") == "xlsx"
 
 def test_file_to_matrix():
-   file_to_matrix_test_array = data_conversion.file_to_matrix("src/filetomatrixtest.csv")
+   file_to_matrix_test_array = data_conversion.file_to_matrix("filetomatrixtest.csv")
    assert file_to_matrix_test_array[0][0] == "hi" 
    assert file_to_matrix_test_array[0][1] == "test" 
    assert file_to_matrix_test_array[1][0] == "hello" 
    assert file_to_matrix_test_array[1][1] == "crazy" 
-   assert data_conversion.file_to_matrix("randomfile.txt") == -1
+   assert data_conversion.file_to_matrix("randomfile.xlsx") == -1
+   result = data_conversion.file_to_matrix("filetomatrixtest2.txt", " ")
+   real_result = np.array([["hello", "darkness", "my"], ["old", "friend", "I've"], ["come", "to", "speak"], ["to", "you", "again"]])
+   assert np.array_equiv(result, real_result)
 
 def test_csv_to_matrix():
-   file_to_matrix_test_array = data_conversion.csv_to_matrix("src/filetomatrixtest.csv")
+   file_to_matrix_test_array = data_conversion.csv_to_matrix("filetomatrixtest.csv")
    assert file_to_matrix_test_array[0][0] == "hi" 
    assert file_to_matrix_test_array[0][1] == "test" 
    assert file_to_matrix_test_array[1][0] == "hello" 
    assert file_to_matrix_test_array[1][1] == "crazy" 
+
+def test_txt_to_matrix():
+   result = data_conversion.txt_to_matrix("filetomatrixtest2.txt", " ")
+   real_result = np.array([["hello", "darkness", "my"], ["old", "friend", "I've"], ["come", "to", "speak"], ["to", "you", "again"]])
+   assert np.array_equiv(result, real_result)
--- src/data_mod/determine_datatype.py
+++ src/data_mod/determine_datatype.py
@@ -31,14 +31,47 @@ def determine_value_datatype(s):
     else:
         return "str"
     
-def determine_column_datatype(col):
+def determine_column_datatype(col, contains_label=False):
     data_types = [determine_value_datatype(s) for s in col]
+    if contains_label:
+        data_types = data_types[1:]
     return mode(data_types)
 
 # returns list of types of each column
 def determine_matrix_datatypes(matrix):
+    contains_labels = matrix_contains_labels(matrix)
     types = [''] * matrix.shape[1]
     for i in range(matrix.shape[1]):
         col = matrix[:, i]
-        types[i] = determine_column_datatype(col)
+        types[i] = determine_column_datatype(col, contains_labels)
     return types
+
+def column_contains_label(col):
+    return determine_value_datatype(col[0]) == "str"
+
+# if a matrix contains labels, we will remove the first row when accounting for types
+def matrix_contains_labels(matrix):
+    for i in range(matrix.shape[1]):
+        col = matrix[:, i]
+        if not column_contains_label(col):
+            return False
+    return True
+
+
+def calculate_numeric_percentage(matrix):
+    used_matrix = matrix
+    if matrix_contains_labels(matrix):
+        used_matrix = matrix[1: , :]
+    count = 0
+    for row in used_matrix:
+        for el in row:
+            if determine_value_datatype(el) == "float" or determine_value_datatype(el) == "int":
+                count += 1
+    return np.round(count/(used_matrix.shape[0] * used_matrix.shape[1]) * 100)/100
+
+def matrix_information_dictionary(matrix):
+    contains_labels = matrix_contains_labels(matrix)
+    datatypes = determine_matrix_datatypes(matrix)
+    numeric_percentage = calculate_numeric_percentage(matrix)
+    information_dictionary = {"labels": contains_labels, "datatypes": datatypes, "numeric percentage": numeric_percentage}
+    return information_dictionary
